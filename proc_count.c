a1bbd4c48050c4f2160b9d83b4c49f6d5f884f21
Pop-source123 <asap332homeboy@gmail.com>
2024-07-05 05:30:25 -0700

    Add files via upload

---
+++ proc_count.c
@@ -0,0 +1,56 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+
+static struct proc_dir_entry *entry;
+
+/*function to keep track of the processes count*/
+static int proc_count(struct seq_file *m, void *v){
+	// TODO: it's all yours
+	struct task_struct *processes;
+	int n = 0;
+	for_each_process(processes) {
+		n++;
+	}
+	seq_printf(m,"%d\n", count);
+	return 0;
+}
+
+/*open function to /proc/count file*/
+static int proc_count_open(struct node *proc_node, struct file *proc_file) {
+	return single_open(proc_file, proc_count, NULL);
+}
+
+/*kernel file operations structure*/
+static const struct file_operations proc_file_operations = {
+    .owner      = THIS_MODULE,
+    .open       = open_process_count_file,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
+}
+
+/*init function for the module*/
+static int __init proc_count_init(void)
+{
+	entry = proc_create_single("count", 0, NULL, proc_count);
+	pr_info("proc_count: init\n");
+	return 0;
+}
+
+/*exit function for the module*/
+static void __exit proc_count_exit(void)
+{
+	proc_remove(entry);
+	pr_info("proc_count: exit\n");
+}
+
+/*metadata*/
+module_init(proc_count_init);
+module_exit(proc_count_exit);
+
+MODULE_AUTHOR("Freddie Kiessling");
+MODULE_DESCRIPTION("CS111 lab0 count proc number");
+MODULE_LICENSE("GPL");
