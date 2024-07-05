0f5aa884a2ac439fdcdf8dabc865d7ecd1ebf8c8
Pop-source123 <asap332homeboy@gmail.com>
2024-07-05 16:17:13 -0700

    Add files via upload

--- proc_count.c
+++ proc_count.c
@@ -16,23 +16,17 @@ static int proc_count(struct seq_file *m, void *v){
 	seq_printf(m,"%d\n", n);
 	return 0;
 }
-
-static int __init proc_count_init(void)
-{
+static int __init proc_count_init(void) {
 	entry = proc_create_single("count", 0, NULL, proc_count);
 	pr_info("proc_count: init\n");
 	return 0;
 }
-
-static void __exit proc_count_exit(void)
-{
+static void __exit proc_count_exit(void) {
 	proc_remove(entry);
 	pr_info("proc_count: exit\n");
 }
-
 module_init(proc_count_init);
 module_exit(proc_count_exit);
-
 MODULE_AUTHOR("Frederick Kiessling");
 MODULE_DESCRIPTION("CS111 lab0 count proc number");
 MODULE_LICENSE("GPL");
